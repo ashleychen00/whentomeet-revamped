d7d2129b7778cf56c9df8bfbc180bca9dfdf3b5e
ashley22 <ashley22@mit.edu>
2021-03-06 17:28:12 -0500

    showBestTimes buggy

--- menu.js
+++ menu.js
@@ -1,3 +1,31 @@
-// aka the content script, run in the context of web pages
 
-alert("Hello from your Chrome extension!!!!!")
+// get the best times
+
+const showBestTimes = () => {
+	// find max number of ppl available at any given point in time
+	var maxNumPpl = AvailableAtSlot.reduce(function (currentMax, slot) {
+		return max(currentMax, slot.length);
+	}, 0);
+
+	// find times where that number of ppl are available
+	var bestTimes = AvailableAtSlot.filter(function (slot) {
+		return slot.length == maxNumPpl;
+	}).map(function (slotIDs, slotIndex) {
+		return slotIndex;
+	});
+
+	// change the highlight around these times
+	console.log("best times " + bestTimes)
+}
+
+// add UI components to main body of site
+
+const mainBody = document.getElementById('MainBody')
+const menu = document.createElement('div')
+menu.style.padding = '1em'
+
+const button = document.createElement('button')
+button.innerText = "Show Best Times"
+button.onclick = () => showBestTimes()
+menu.appendChild(button)
+mainBody.appendChild(menu)
