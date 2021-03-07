9435efc6ce2e039b8372fede6383c28135eaf598
ashley22 <ashley22@mit.edu>
2021-03-07 12:51:26 -0500

    in conclusion we hate js filter reduce

--- menu.js
+++ menu.js
@@ -3,19 +3,25 @@
 
 const showBestTimes = () => {
 	// find max number of ppl available at any given point in time
-	var maxNumPpl = AvailableAtSlot.reduce(function (currentMax, slot) {
-		return max(currentMax, slot.length);
-	}, 0);
+	var maxNumPpl = 0;
+	for (var i = 0; i < AvailableAtSlot.length; i++) {
+		if (AvailableAtSlot[i].length > maxNumPpl) {
+			maxNumPpl = AvailableAtSlot[i].length;
+		}
+	}
+
+	console.log("max num: ", maxNumPpl)
 
 	// find times where that number of ppl are available
-	var bestTimes = AvailableAtSlot.filter(function (slot) {
-		return slot.length == maxNumPpl;
-	}).map(function (slotIDs, slotIndex) {
-		return slotIndex;
-	});
+	var bestTimes = new Array();
+	for (var j = 0; j < AvailableAtSlot.length; j++) {
+		if (AvailableAtSlot[j].length == maxNumPpl) {
+			bestTimes.push(j);
+		}
+	}
 
 	// change the highlight around these times
-	console.log("best times " + bestTimes)
+	console.log("best times ", bestTimes)
 }
 
 // add UI components to main body of site
